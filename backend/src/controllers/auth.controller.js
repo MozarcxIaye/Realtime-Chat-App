5fc8bcf273c773e45bd592f40d4d74dcc990edde
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-05-13 17:26:53 +0545

    created update Profile controller and cloudinary setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async(req, res) => {
 
@@ -111,4 +112,32 @@ export const logout = (req, res) => {
         })
         
     }
+}
+
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic){
+            res.status(400).json({
+                message: "Profile Pic is required."
+            })
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true})
+
+        res.status(200).json({
+            updatedUser
+        })
+        
+    } catch (error) {
+        console.log("Error in the updateProfile Controller.", error)
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,19 @@ import express from 'express'
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import { connectDB } from './lib/db.js'
+import cookieParser from "cookie-parser"
 
 
-dotenv.config()
-const app = express() 
-const PORT = process.env.PORT
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
 
 
-app.use(express.json())
-app.use("/api/auth", authRoutes)
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=> {
-    console.log(`App running on ${PORT} port.`)
-    connectDB()
-})
+    console.log(`App running on ${PORT} port.`);
+    connectDB();
+});
