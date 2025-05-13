267f2a0865c684aafeb31e3834856aa257670d96
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-05-13 14:36:27 +0545

    created logout controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,5 +98,17 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    res.send("Logout route")
+    try {
+        res.cookie("jwt", "", {maxAge: 0})
+        res.status(200).json({
+            message: "Logged out succesfully."
+        })
+        
+    } catch (error) {
+        console.log("Error in the logout Controller.", error)
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+        
+    }
 }
