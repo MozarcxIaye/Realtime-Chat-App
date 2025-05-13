ca4c5e20e27a42f2f989683374b4471dbb69a6d2
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-05-13 10:19:00 +0545

    created signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,62 @@
-export const signup = (req, res) => {
-    res.send("Signup route")
+import { generateToken } from "../lib/utils.js"
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async(req, res) => {
+
+    const {fullName, email, password} = req.body
+    try {
+
+        if(!fullName || !email || !password) {
+            return res.status(400).json({
+                message: "Please fill all the required fields."
+            })
+        }
+
+        if(password.length < 8) {
+            return res.status(400).json({
+                message: "Password too short. Should be at least 8 characters."
+            })
+        }        
+
+        const user = await User.findOne({email})
+        if(user) return res.status(400).json({
+            message: "Email already taken. Please try with another one."
+        })
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword 
+        })
+
+        if(newUser) {
+            // generate jwt token
+            generateToken(newUser._id, res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+
+        } else {
+            res.status(400).json({
+                message: "Invalid User Data"
+            })
+        }
+        
+    } catch (error) {
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
 }
 
 export const login = (req, res) => {
