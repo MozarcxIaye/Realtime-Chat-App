11e6279897bceac3f7f7b920a2048c46f56e3214
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2025-07-02 22:14:23 +0545

    added a loader and protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import Navbar from "./components/Navbar"
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import LoginPage from "./pages/LoginPage"
 import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import SignUpPage from "./pages/SignUpPage"
+import { useAuthStore } from "./store/useAuthStore"
+import { useEffect } from "react"
+import {LoaderCircle} from "lucide-react"
 
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  console.log({authUser})
+
+  if(isCheckingAuth && authUser) return (
+
+    <div className="flex items-center justify-center h-screen gap-2">
+      <LoaderCircle className="animate-spin size-10" />
+      <span>Loading...</span>
+    </div>
+
+
+  )
+
   return (
     <div>
 
@@ -15,11 +36,11 @@ const App = () => {
 
       {/* Routes */}
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <Homepage />: <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage />: <Navigate to="/login" />} />
 
       </Routes>
 
